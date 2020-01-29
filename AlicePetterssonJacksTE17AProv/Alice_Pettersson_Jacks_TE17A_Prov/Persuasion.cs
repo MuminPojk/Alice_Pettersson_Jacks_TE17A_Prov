48647113f22a2a4ef1d27e3590faf42afada3fa3
MuminPojk <54946936+MuminPojk@users.noreply.github.com>
2020-01-29 14:55:53 +0100

    First commit

---
+++ AlicePetterssonJacksTE17AProv/Alice_Pettersson_Jacks_TE17A_Prov/Persuasion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alice_Pettersson_Jacks_TE17A_Prov
+{
+    class Persuasion: MentalPower
+    {
+    }
+}
---
+++ AlicePetterssonJacksTE17AProv/Alice_Pettersson_Jacks_TE17A_Prov/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alice_Pettersson_Jacks_TE17A_Prov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the yearly superhero competition, what is your name?");
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Okay, " + input + " that is a nice name!");
+            Console.WriteLine("What kind of power do you have? Is it a physical or mental one?");
+
+        
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
